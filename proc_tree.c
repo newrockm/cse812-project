8abdb6c9a5239206ef4eef565da729e657a9753d
newrockm <newrockm@cse812-05.cse.msu.edu>
2013-04-04 17:04:42 -0400

    Log to the kernel log (dmesg) when reading the directory.

--- proc_tree.c
+++ proc_tree.c
@@ -10,10 +10,28 @@
 
 MODULE_LICENSE("GPL");
 
+struct proc_dir_entry *proc_tree_root;
 
+
+/* directory read */
+int proc_tree_readdir(struct file * filp, void * dirent, filldir_t filldir)
+{
+	printk(KERN_INFO "Called proc_tree_readdir.\n");
+	return 0;
+}
+
+static const struct file_operations proc_tree_file_operations = {
+	.read		= generic_read_dir,
+	.readdir	= proc_tree_readdir,
+};
+
+
+/* startup and shutdown */
 int proc_tree_init(void)
 {
-	proc_mkdir("tree", NULL);
+	proc_tree_root = proc_mkdir("tree", NULL);
+	
+	proc_tree_root->proc_fops = &proc_tree_file_operations;
 	return 0;
 }
 module_init(proc_tree_init);
