d47cf25ea051920e64726f8beeee31f3830b83be
Marshal Newrock <newrockm@cse.msu.edu>
2013-04-10 02:33:08 -0400

    Trying module again, using different approaches. Sample file in /proc/tree which has the current time as the filename.

--- proc_tree.c
+++ proc_tree.c
@@ -6,7 +6,9 @@
 #include <linux/kernel.h>   /* We're doing kernel work */
 #include <linux/module.h>   /* Specifically, a module */
 
+#include <linux/fs.h>
 #include <linux/proc_fs.h>
+#include <linux/time.h>
 
 MODULE_LICENSE("GPL");
 
@@ -16,15 +18,55 @@ struct proc_dir_entry *proc_tree_root;
 /* directory read */
 int proc_tree_readdir(struct file * filp, void * dirent, filldir_t filldir)
 {
-	printk(KERN_INFO "Called proc_tree_readdir.\n");
-	return 0;
+	int i;
+	struct dentry *dentry = filp->f_path.dentry;
+	struct inode *inode = dentry->d_inode;
+	ino_t ino;
+	int ret;
+
+	struct timespec uptime;
+	char uptime_fname[16];
+	int len;
+
+	i = filp->f_pos;
+	switch (i){
+	case 0:
+		ino = inode->i_ino;
+		if (filldir(dirent, ".", 1, i, ino, DT_DIR) < 0)
+			goto out;
+		i++;
+		filp->f_pos++;
+		/* fall through */
+	case 1:
+		ino = parent_ino(dentry);
+		if (filldir(dirent, "..", 2, i, ino, DT_DIR) < 0)
+			goto out;
+		i++;
+		filp->f_pos++;
+		/* fall through */
+	case 2:
+		uptime = current_kernel_time();
+		snprintf(uptime_fname, 15, "%d", (int)uptime.tv_sec);
+		len = strlen(uptime_fname);
+
+		ino = iunique(inode->i_sb, 2);
+		if (filldir(dirent, uptime_fname, len, i, ino, DT_REG) < 0)
+			goto out;
+		i++;
+		filp->f_pos++;
+		break;
+
+	}
+
+	ret = 1;
+out:
+	return ret;
 }
 
 static const struct file_operations proc_tree_file_operations = {
 	.read		= generic_read_dir,
 	.readdir	= proc_tree_readdir,
 };
-
 
 /* startup and shutdown */
 int proc_tree_init(void)
