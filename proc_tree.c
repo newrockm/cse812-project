01a668c2b12d3268877364d68b31c37a3aacc2dd
Marshal Newrock <newrockm@cse.msu.edu>
2013-04-18 18:51:31 -0400

    Create a basic /proc/tree file with a message when read.

--- proc_tree.c
+++ proc_tree.c
@@ -6,74 +6,36 @@
 #include <linux/kernel.h>   /* We're doing kernel work */
 #include <linux/module.h>   /* Specifically, a module */
 
-#include <linux/fs.h>
 #include <linux/proc_fs.h>
-#include <linux/time.h>
 
 MODULE_LICENSE("GPL");
 
-struct proc_dir_entry *proc_tree_root;
+static struct proc_dir_entry *proc_tree_file;
 
-
-/* directory read */
-int proc_tree_readdir(struct file * filp, void * dirent, filldir_t filldir)
+int proc_tree_read(char *buffer, char **buffer_location, off_t offset, 
+		int buffer_length, int *eof, void *data)
 {
-	int i;
-	struct dentry *dentry = filp->f_path.dentry;
-	struct inode *inode = dentry->d_inode;
-	ino_t ino;
 	int ret;
 
-	struct timespec uptime;
-	char uptime_fname[16];
-	int len;
-
-	i = filp->f_pos;
-	switch (i){
-	case 0:
-		ino = inode->i_ino;
-		if (filldir(dirent, ".", 1, i, ino, DT_DIR) < 0)
-			goto out;
-		i++;
-		filp->f_pos++;
-		/* fall through */
-	case 1:
-		ino = parent_ino(dentry);
-		if (filldir(dirent, "..", 2, i, ino, DT_DIR) < 0)
-			goto out;
-		i++;
-		filp->f_pos++;
-		/* fall through */
-	case 2:
-		uptime = current_kernel_time();
-		snprintf(uptime_fname, 15, "%d", (int)uptime.tv_sec);
-		len = strlen(uptime_fname);
-
-		ino = iunique(inode->i_sb, 2);
-		if (filldir(dirent, uptime_fname, len, i, ino, DT_REG) < 0)
-			goto out;
-		i++;
-		filp->f_pos++;
-		break;
-
+	if (offset > 0) {
+		/* we have finished reading, return 0 */
+		ret = 0;
+	}
+	else {
+		ret = sprintf(buffer, "Welcome to the proc_tree module!\n");
 	}
 
-	ret = 1;
-out:
 	return ret;
 }
 
-static const struct file_operations proc_tree_file_operations = {
-	.read		= generic_read_dir,
-	.readdir	= proc_tree_readdir,
-};
 
 /* startup and shutdown */
 int proc_tree_init(void)
 {
-	proc_tree_root = proc_mkdir("tree", NULL);
-	
-	proc_tree_root->proc_fops = &proc_tree_file_operations;
+	proc_tree_file = create_proc_entry("tree", 0644, NULL);
+
+	proc_tree_file->read_proc = proc_tree_read;
+
 	return 0;
 }
 module_init(proc_tree_init);
