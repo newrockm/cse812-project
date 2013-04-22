ee6607baed69acd75b953c251b5981528b527ac2
Marshal Newrock <newrockm@cse.msu.edu>
2013-04-22 02:38:19 -0400

    Code cleanup and reorg, add comments.

--- proc_tree.c
+++ proc_tree.c
@@ -6,7 +6,6 @@
 #include <linux/kernel.h>   /* We're doing kernel work */
 #include <linux/module.h>   /* Specifically, a module */
 
-#include <linux/list.h>
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
 
@@ -17,6 +16,18 @@ MODULE_LICENSE("GPL");
 
 #define PROC_TREE_OUTPUT_SIZE 4096
 
+static struct proc_dir_entry *proc_tree_file;
+/*char *proc_tree_output; */
+char proc_tree_output[PROC_TREE_OUTPUT_SIZE];
+int proc_tree_output_size_used = 1;
+
+
+/* structures and functions to hold a heirarchical task list */
+
+/** 
+ * Heirarchical process list.  New entries are added to the end of the
+ * sibling list at the appropriate depth (under the appropriate parent).
+ */
 struct proc_tree_list_struct {
 	struct task_struct *task;
 	struct proc_tree_list_struct *first_child;
@@ -24,24 +35,29 @@ struct proc_tree_list_struct {
 	struct proc_tree_list_struct *parent;
 };
 
+/** 
+ * A linear list pointing at the heirarchical list, for easy access.  New
+ * entries are added to the beginning of this list.  Nothing fancy here.
+ */
 struct proc_tree_list_seq_struct {
 	struct task_struct *task;
 	struct proc_tree_list_struct *entry;
 	struct proc_tree_list_seq_struct *next;
 };
 
+
 static struct proc_tree_list_struct *proc_tree_list = NULL;
 static struct proc_tree_list_seq_struct *proc_tree_list_seq = NULL;
 
-static struct proc_dir_entry *proc_tree_file;
-/*char *proc_tree_output; */
-char proc_tree_output[PROC_TREE_OUTPUT_SIZE];
-int proc_tree_output_size_used = 1;
-
+/**
+ * Find the entry for the specified parent task from the heirarchical list.
+ * @parent: the parent task
+ */
 struct proc_tree_list_struct *proc_tree_find_parent(struct task_struct *parent) {
 	struct proc_tree_list_seq_struct *seq = proc_tree_list_seq;
 	struct proc_tree_list_struct *found = NULL;
 
+	// walk the sequential list to find the parent list entry.
 	while (seq != NULL) {
 		if (seq->task == parent) {
 			found = seq->entry;
@@ -53,6 +69,10 @@ struct proc_tree_list_struct *proc_tree_find_parent(struct task_struct *parent) 
 	return found;
 }
 
+/**
+ * Add a new entry into both the heirarchical and sequential lists.
+ * @task: the new task to create entries for.
+ */
 void proc_tree_add_entry(struct task_struct *task) {
 	struct proc_tree_list_struct *parent_entry, *sibling_entry, *new_entry;
 	struct proc_tree_list_seq_struct *seq;
@@ -78,25 +98,26 @@ void proc_tree_add_entry(struct task_struct *task) {
 	seq->entry = new_entry;
 
 	if (proc_tree_list == NULL) {
+		// first entry
 		proc_tree_list = new_entry;
 		seq->next = NULL;
 		proc_tree_list_seq = seq;
 	}
 	else {
-		// no parent
 		if (parent_entry == NULL) {
+			// no parent
 			sibling_entry = proc_tree_list;
 			while (sibling_entry->sibling != NULL) {
 				sibling_entry = sibling_entry->sibling;
 			}
 			sibling_entry->sibling = new_entry;
 		}
-		// parent has no children
 		else if (parent_entry->first_child == NULL) {
+			// parent has no children
 			parent_entry->first_child = new_entry;
 		}
-		// parent exists, has children
 		else {
+			// parent exists, has children
 			sibling_entry = parent_entry->first_child;
 			while (sibling_entry->sibling != NULL) {
 				sibling_entry = sibling_entry->sibling;
@@ -109,6 +130,10 @@ void proc_tree_add_entry(struct task_struct *task) {
 	}
 }
 
+/**
+ * Clear the stored list of processes, both the heirarchical list and the
+ * sequential list.
+ */
 void proc_tree_clear_list(void) {
 	struct proc_tree_list_struct *entry;
 	struct proc_tree_list_seq_struct *seq, *next;
@@ -126,6 +151,11 @@ void proc_tree_clear_list(void) {
 	proc_tree_list_seq = NULL;
 }
 
+/**
+ * Go through the heirarchical process list, starting at the beginning.
+ * For each process, get info about that process, then the process's children,
+ * then the process's siblings, then back to the parent if it exists.
+ */
 void proc_tree_walk_list(void) {
 	struct proc_tree_list_struct *entry = proc_tree_list;
 	struct task_struct *task;
@@ -136,15 +166,18 @@ void proc_tree_walk_list(void) {
 		printk("(%d) %d: %s\n", depth, task->pid, task->comm);
 
 		if (entry->first_child != NULL) {
+			// process children.
 			depth++;
 			entry = entry->first_child;
 		}
 		else if (entry->sibling != NULL) {
+			// process next sibling.
 			entry = entry->sibling;
 		}
 		else if (entry->parent != NULL) {
 			// we've already done the immediate parent.
 			// get the parent's sibling.
+			depth--;
 			entry = entry->parent->sibling;
 		}
 		else {
@@ -154,6 +187,7 @@ void proc_tree_walk_list(void) {
 		}
 	}
 }
+
 
 void proc_tree_clear_output(void) {
 	/*
@@ -188,85 +222,6 @@ void proc_tree_format_output(struct task_struct *task, int depth) {
 	proc_tree_add_output(buffer);
 }
 
-void proc_tree_get_tasks(struct task_struct *start_task, int depth)
-{
-	struct task_struct *current_task, *child;
-	struct list_head *t_list;
-
-	// safety switch
-	if (depth > 3) {
-		printk(KERN_ERR "Too much recursion in proc_tree_get_tasks\n");
-		return;
-	}
-
-	if (start_task == NULL || strlen(start_task->comm) == 0) {
-		printk("Invalid task found.\n");
-	}
-
-	//printk("start: %d: %d: %s\n", depth, start_task->pid, start_task->comm);
-	//proc_tree_format_output(start_task, depth);
-
-	/* find task children */
-	list_for_each(t_list, &start_task->children) {
-		child = list_entry(t_list, struct task_struct, children);
-		if (strlen(child->comm) == 0) {
-			printk("Invalid child process entry found.\n");
-			continue;
-		}
-		printk("child: %d: %d: %s\n", depth, child->pid, child->comm);
-
-		/*
-		grandchild = child_task(child);
-		if (strlen(grandchild->comm) > 0) {
-			proc_tree_get_tasks(grandchild, 
-		*/
-	}
-
-	/*
-	child = child_task(start_task);
-	t_list = child->sibling.next;
-	if (!list_is_last(t_list, &child->sibling)) {
-	//if (!list_is_singular(&start_task->children)) {
-		proc_tree_get_tasks(child, depth + 1);
-	}
-	*/
-
-	//list_for_each_entry(current_task, &start_task->sibling, sibling) {
-	list_for_each(t_list, &start_task->sibling) {
-		current_task = list_entry(t_list, struct task_struct, sibling);
-		if (strlen(current_task->comm) == 0) {
-			printk("Invalid sibling process entry found.\n");
-			continue;
-		}
-		if (list_is_last(t_list, &start_task->sibling)) {
-			printk("Last sibling.\n");
-			continue;
-		}
-		printk("sibling: %d: %d: %s\n", depth, current_task->pid, current_task->comm);
-		child = child_task(current_task);
-		proc_tree_get_tasks(child, depth + 1);
-		/*if (!list_empty(&current_task->children)) {
-			child = child_task(current_task);
-			proc_tree_get_tasks(child, depth + 1);
-		}
-		else {
-			printk("%d: %d: %s\n", depth, current_task->pid, current_task->comm);
-		}
-		*/
-	}
-	/*
-	current_task = task_sibling(start_task);
-	while (current_task != start_task) {
-		next_task = task_sibling(current_task);
-		if (next_task != start_task) {
-			printk("%d: %d: %s\n", depth, current_task->pid, current_task->comm);
-		}
-		//proc_tree_format_output(current_task, depth);
-		current_task = next_task;
-	}
-	*/
-}
-
 
 int proc_tree_read(char *buffer, char **buffer_location, off_t offset, 
 		int buffer_length, int *eof, void *data)
@@ -279,13 +234,7 @@ int proc_tree_read(char *buffer, char **buffer_location, off_t offset,
 		ret = 0;
 	}
 	else {
-		//proc_tree_clear_output();
-		// task 0 is the swapper, let's start at task 1 (init)
-		//proc_tree_get_tasks(next_task(&init_task), 0);
-		//proc_tree_get_tasks(&init_task, 0);
-		//strncpy(buffer, proc_tree_output, buffer_length - 1);
-		//ret = strlen(buffer);
-
+		// go through all processes, store in the heirarchical struct.
 		for_each_process(task) {
 			proc_tree_add_entry(task);
 		}
@@ -303,9 +252,7 @@ int proc_tree_read(char *buffer, char **buffer_location, off_t offset,
 int proc_tree_init(void)
 {
 	proc_tree_file = create_proc_entry("tree", 0644, NULL);
-
 	proc_tree_file->read_proc = proc_tree_read;
-
 	return 0;
 }
 module_init(proc_tree_init);
